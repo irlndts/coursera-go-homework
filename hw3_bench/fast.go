ab1111e9b945d1bc3703b303892e044195d2e27c
irlndts <kleto4kin@gmail.com>
2018-02-15 21:02:00 +0300

    WIP

--- hw3_bench/fast.go
+++ hw3_bench/fast.go
@@ -8,14 +8,32 @@ import (
 	"os"
 	"strings"
 
-	"github.com/irlndts/coursera-go-homework/hw3_bench/json"
+	easyjson "github.com/mailru/easyjson"
+	jlexer "github.com/mailru/easyjson/jlexer"
+	jwriter "github.com/mailru/easyjson/jwriter"
 )
 
+// suppress unused package warning
+var (
+	_ *json.RawMessage
+	_ *jlexer.Lexer
+	_ *jwriter.Writer
+	_ easyjson.Marshaler
+)
+
+//easyjson:json
+type User struct {
+	Browsers []string `json:"browsers"`
+	Email    string   `json:"email"`
+	Name     string   `json:"name"`
+}
+
 func FastSearch(out io.Writer) {
-	file, err := os.Open(filePath)
+	file, err := os.OpenFile(filePath, os.O_RDONLY, 0400)
 	if err != nil {
 		panic(err)
 	}
+	defer file.Close()
 
 	scanner := bufio.NewScanner(file)
 	if err != nil {
@@ -24,10 +42,19 @@ func FastSearch(out io.Writer) {
 
 	seenBrowsers := make(map[string]bool)
 
+	/*
+		var dataPool = sync.Pool{
+			New: func() interface{} {
+				return &User{}
+			},
+		}
+	*/
+
 	index := -1
-	user := &myjson.User{}
 	var isAndroid, isMSIE bool
 	var email string
+
+	user := &User{}
 
 	fmt.Fprintln(out, "found users:")
 	for scanner.Scan() {
@@ -41,16 +68,12 @@ func FastSearch(out io.Writer) {
 		isMSIE = false
 
 		for _, browser := range user.Browsers {
-			if strings.Contains(browser, "Android") {
-				isAndroid = true
-				if !seenBrowsers[browser] {
-					seenBrowsers[browser] = true
-				}
-			} else if strings.Contains(browser, "MSIE") {
+			if strings.Contains(browser, "MSIE") {
 				isMSIE = true
-				if !seenBrowsers[browser] {
-					seenBrowsers[browser] = true
-				}
+				seenBrowsers[browser] = true
+			} else if strings.Contains(browser, "Android") {
+				isAndroid = true
+				seenBrowsers[browser] = true
 			}
 		}
 
@@ -63,4 +86,132 @@ func FastSearch(out io.Writer) {
 	}
 
 	fmt.Fprintln(out, "\nTotal unique browsers", len(seenBrowsers))
+}
+
+func easyjson3486653aDecodeGithubComIrlndtsCourseraGoHomeworkHw3Bench(in *jlexer.Lexer, out *User) {
+	isTopLevel := in.IsStart()
+	if in.IsNull() {
+		if isTopLevel {
+			in.Consumed()
+		}
+		in.Skip()
+		return
+	}
+	in.Delim('{')
+	for !in.IsDelim('}') {
+		key := in.UnsafeString()
+		in.WantColon()
+		if in.IsNull() {
+			in.Skip()
+			in.WantComma()
+			continue
+		}
+		switch key {
+		case "browsers":
+			if in.IsNull() {
+				in.Skip()
+				out.Browsers = nil
+			} else {
+				in.Delim('[')
+				if out.Browsers == nil {
+					if !in.IsDelim(']') {
+						out.Browsers = make([]string, 0, 4)
+					} else {
+						out.Browsers = []string{}
+					}
+				} else {
+					out.Browsers = (out.Browsers)[:0]
+				}
+				for !in.IsDelim(']') {
+					var v1 string
+					v1 = string(in.String())
+					out.Browsers = append(out.Browsers, v1)
+					in.WantComma()
+				}
+				in.Delim(']')
+			}
+		case "email":
+			out.Email = string(in.String())
+		case "name":
+			out.Name = string(in.String())
+		default:
+			in.SkipRecursive()
+		}
+		in.WantComma()
+	}
+	in.Delim('}')
+	if isTopLevel {
+		in.Consumed()
+	}
+}
+func easyjson3486653aEncodeGithubComIrlndtsCourseraGoHomeworkHw3Bench(out *jwriter.Writer, in User) {
+	out.RawByte('{')
+	first := true
+	_ = first
+	{
+		const prefix string = ",\"browsers\":"
+		if first {
+			first = false
+			out.RawString(prefix[1:])
+		} else {
+			out.RawString(prefix)
+		}
+		if in.Browsers == nil && (out.Flags&jwriter.NilSliceAsEmpty) == 0 {
+			out.RawString("null")
+		} else {
+			out.RawByte('[')
+			for v2, v3 := range in.Browsers {
+				if v2 > 0 {
+					out.RawByte(',')
+				}
+				out.String(string(v3))
+			}
+			out.RawByte(']')
+		}
+	}
+	{
+		const prefix string = ",\"email\":"
+		if first {
+			first = false
+			out.RawString(prefix[1:])
+		} else {
+			out.RawString(prefix)
+		}
+		out.String(string(in.Email))
+	}
+	{
+		const prefix string = ",\"name\":"
+		if first {
+			first = false
+			out.RawString(prefix[1:])
+		} else {
+			out.RawString(prefix)
+		}
+		out.String(string(in.Name))
+	}
+	out.RawByte('}')
+}
+
+// MarshalJSON supports json.Marshaler interface
+func (v User) MarshalJSON() ([]byte, error) {
+	w := jwriter.Writer{}
+	easyjson3486653aEncodeGithubComIrlndtsCourseraGoHomeworkHw3Bench(&w, v)
+	return w.Buffer.BuildBytes(), w.Error
+}
+
+// MarshalEasyJSON supports easyjson.Marshaler interface
+func (v User) MarshalEasyJSON(w *jwriter.Writer) {
+	easyjson3486653aEncodeGithubComIrlndtsCourseraGoHomeworkHw3Bench(w, v)
+}
+
+// UnmarshalJSON supports json.Unmarshaler interface
+func (v *User) UnmarshalJSON(data []byte) error {
+	r := jlexer.Lexer{Data: data}
+	easyjson3486653aDecodeGithubComIrlndtsCourseraGoHomeworkHw3Bench(&r, v)
+	return r.Error()
+}
+
+// UnmarshalEasyJSON supports easyjson.Unmarshaler interface
+func (v *User) UnmarshalEasyJSON(l *jlexer.Lexer) {
+	easyjson3486653aDecodeGithubComIrlndtsCourseraGoHomeworkHw3Bench(l, v)
 }
--- hw3_bench/json/user.go
+++
@@ -1,8 +0,0 @@
-package myjson
-
-//easyjson:json
-type User struct {
-	Browsers []string `json:"browsers"`
-	Email    string   `json:"email"`
-	Name     string   `json:"name"`
-}
