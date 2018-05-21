a50cf26a5736d5c54c60b4f412c6cb91b14085bf
irlndts <kleto4kin@gmail.com>
2018-05-21 20:36:27 +0300

    hw_5 work in progress

--- hw5_codegen/handlers_gen/codegen.go
+++ hw5_codegen/handlers_gen/codegen.go
@@ -1,3 +1,26 @@
 package main
 
-// код писать тут
+import (
+	"fmt"
+	"go/parser"
+	"go/token"
+	"log"
+	"os"
+)
+
+func main() {
+	fset := token.NewFileSet()
+	node, err := parser.ParseFile(fset, os.Args[1], nil, parser.ParseComments)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	out, _ := os.Create(os.Args[2])
+
+	fmt.Fprintln(out, `package `+node.Name.Name)
+	fmt.Fprintln(out) // empty line
+	//fmt.Fprintln(out, `import "encoding/binary"`)
+	//fmt.Fprintln(out, `import "bytes"`)
+	//fmt.Fprintln(out) // empty line
+
+}
